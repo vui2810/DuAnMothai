956fa1cb7fac3c7bc4757c426b6da2bb35455f50
ADMIN <ADMIN@PC219>
2024-02-27 07:33:45 +0700

    Add project files.

---
+++ DuAnMot/ThongTinCaNhan.aspx
@@ -0,0 +1,90 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ThongTinCaNhan.aspx.cs" Inherits="DuAnMot.ThongTinCaNhan" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="Content/bootstrap.min.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+      
+        <div class="w-50 m-auto">
+            <h2 class="text-center">THÔNG TIN CÁ NHÂN</h2>
+            <hr />
+
+            <div class="row mb-2">
+                <div class="col-2">
+                    Họ tên
+                </div>
+                <div class="col-8">
+                    <asp:TextBox ID="txtHoTen" runat="server" CssClass="form-control"></asp:TextBox>
+                </div>
+            </div>
+            <div class="row mb-2">
+                <div class="col-2">
+                    Ngày sinh
+                </div>
+                <div class="col-8">
+                    <asp:TextBox ID="txtNgaySinh" runat="server" TextMode="Date" CssClass="form-control"></asp:TextBox>
+                </div>
+            </div>
+            <div class="row mb-2">
+                <div class="col-2">
+                    Giới tính
+                </div>
+                <div class="col-8">
+                    <asp:RadioButton ID="rdNam" Text="Nam" runat="server" GroupName="GT" />
+                    <asp:RadioButton ID="rdNu" Text="Nữ" runat="server" GroupName="GT" />
+                </div>
+            </div>
+            <div class="row mb-2">
+                <div class="col-2">
+                    Trình độ
+                </div>
+                <div class="col-8">
+                    <asp:DropDownList ID="ddlTrinhDo" runat="server" CssClass="form-control"></asp:DropDownList>
+                </div>
+            </div>
+             <div class="row mb-2">
+                <div class="col-2">
+                    Nghề nghiệp
+                </div>
+                <div class="col-8">
+                    <asp:ListBox ID="lstNgheNghiep" runat="server" CssClass="form-control"></asp:ListBox>
+                </div>
+            </div>
+             <div class="row mb-2">
+                <div class="col-2">
+                   Hình
+                </div>
+                <div class="col-8">
+                    <asp:FileUpload ID="FHinh" runat="server"  CssClass="form-control" />
+                </div>
+            </div>
+             <div class="row mb-2">
+                <div class="col-2">
+                   Sở thích
+                </div>
+                <div class="col-8">
+                    <asp:CheckBoxList ID="chkListSoThich" runat="server"></asp:CheckBoxList>
+                </div>
+            </div>
+
+             <div class="row mb-2">
+                 <div class="col-2">
+                 </div>
+                <div class="col-8">
+                    <asp:Button ID="btGui" runat="server" OnClick="btGui_Click1" CssClass="btn btn-primary mr-4" Text="Gửi" />
+                    <asp:Button ID="btLamLai" runat="server" OnClick="btGui_Click1" CssClass="btn btn-info" Text="Làm lại" />
+                </div>
+            </div>
+            <div class="col-8">
+                    <asp:Literal ID="lbKetQua" runat="server"></asp:Literal>
+                </div>
+            </div>
+        </div>       
+    </form>
+</body>
+</html>
---
+++ DuAnMot/ThongTinCaNhan.aspx.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DuAnMot
+{
+    public partial class ThongTinCaNhan : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //code khoi tao du lieu cho cac dieu kien
+            if (!Page.IsPostBack)
+            {
+            //khoi tao cho ddlTrinhDo
+            ddlTrinhDo.Items.Add(new ListItem("Trung cấp"));
+            ddlTrinhDo.Items.Add(new ListItem("Cao đẳng"));
+            //...
+
+            //khoi tao cho lstNgheNghiep
+            lstNgheNghiep.Items.Add(new ListItem("Công nhân"));
+            lstNgheNghiep.Items.Add(new ListItem("Lập trình viên"));
+            lstNgheNghiep.Items.Add(new ListItem("Kỹ sư"));
+            //...
+
+            //khoi tao cho chkListSoThich
+            chkListSoThich.Items.Add(new ListItem("Mua sắm"));
+            chkListSoThich.Items.Add(new ListItem("Xem phim"));
+            chkListSoThich.Items.Add(new ListItem("Du lịch"));
+            chkListSoThich.Items.Add(new ListItem("Leo Núi"));
+            ///...
+            }
+        }
+        protected void btGui_Click(object sender, EventArgs e)
+        {
+            string kq = "";
+            //b1 thu thap tt
+            kq += "<h2>Thông tin đăng ký của bạn </h2>";
+            kq += "<ul>";
+            //lay tt ho ten
+            kq += $"<li>Họ tên {txtHoTen.Text}</li>";
+            //tuong tu cho cac tt khac
+            kq += string.Format("<li> Ngày Sinh:{0}</li>", txtNgaySinh.Text);
+            if (rdNam.Checked)
+            {
+                kq += $"<li> Giới tính {rdNam.Text} </li>";
+            }
+            else
+            {
+                kq += $"<li> Giới tính {rdNu.Text} </li>";
+            }
+            kq += $"<li>Trình độ {ddlTrinhDo.SelectedItem.Text}</li>";
+            kq += $"<li>Nghề nghiệp {lstNgheNghiep.SelectedItem.Text}</li>";
+            // xưu lý upload
+            if (FHinh.HasFile)
+            {
+                //lấy về đường dẫn tuyệt đối các thu mục tren sever
+                string path = Server.MapPath("~/uploads");
+                //sao chep tap tin ttu người dung len server
+                FHinh.SaveAs(path + "/" + FHinh.FileName);
+                kq += $"<li>Ảnh : <img src='Uploads/{FHinh.FileName}'> </li>";
+            }
+            kq += "<ul>";
+
+            //b2 tra ket qua ve client
+            lbKetQua.Text = kq;
+        }
+
+        protected void btGui_Click1(object sender, EventArgs e)
+        {
+            string kq = "";
+            //b1 thu thap tt
+            kq += "<h2>Thông tin đăng ký của bạn </h2>";
+            kq += "<ul>";
+            //lay tt ho ten
+            kq += $"<li>Họ tên {txtHoTen.Text}</li>";
+            //tuong tu cho cac tt khac
+            kq += string.Format("<li> Ngày Sinh:{0}</li>", txtNgaySinh.Text);
+            if (rdNam.Checked)
+            {
+                kq += $"<li> Giới tính {rdNam.Text} </li>";
+            }
+            else
+            {
+                kq += $"<li> Giới tính {rdNu.Text} </li>";
+            }
+            kq += $"<li>Trình độ {ddlTrinhDo.SelectedItem.Text}</li>";
+            kq += $"<li>Nghề nghiệp {lstNgheNghiep.SelectedItem.Text}</li>";
+            // xưu lý upload
+            if (FHinh.HasFile)
+            {
+                //lấy về đường dẫn tuyệt đối các thu mục tren sever
+                string path = Server.MapPath("~/uploads");
+                //sao chep tap tin ttu người dung len server
+                FHinh.SaveAs(path + "/" + FHinh.FileName);
+                kq += $"<li>Ảnh : <img src='Uploads/{FHinh.FileName}'> </li>";
+            }
+            string sothich = " ";
+            foreach (ListItem x in chkListSoThich.Items)
+            {
+                if (x.Selected)
+                {
+                    sothich += x.Text + ";";
+                }
+            }
+            kq += $"<li>Sở thích: {sothich}</li>";
+            kq += "<ul>";
+
+            //b2 tra ket qua ve client
+            lbKetQua.Text = kq;
+        }
+    }
+}
+
+
+          
